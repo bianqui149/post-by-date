0ba8b5ac85d4bd1e113833e5c675df9878eb7eb8
julian bianqui <bianquijulian@gmail.com>
2024-10-01 09:56:55 -0300

    initial commit

---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5aed8f20ed225ef8503a');
---
+++ post-by-date.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:       Post By Date
+ * Description:       Display a list of posts from a category
+ * Requires at least: 6.6
+ * Requires PHP:      7.4.0
+ * Version:           0.1.0
+ * Author:            Bianqui Julian
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       post-by-date
+ *
+ * @package CreateBlock
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_post_by_date_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_post_by_date_block_init' );
