5b44358d99227558d609745062f3d9a0bad1e2a7
julian bianqui <bianquijulian@gmail.com>
2024-10-02 15:31:33 -0300

    fix(lint): apply automatic fixes for CSS linting errors
    Clean up CSS code to adhere to style guidelines.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '1fb59acd159d3e883af5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'f9a7a36614d84f7c0ef2');
